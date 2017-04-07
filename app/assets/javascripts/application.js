b0c622f8247201534bf02e6fafbdd6ceab7c9495
PaoloVeloce <pablo.veloce@gmail.com>
2017-04-08 00:00:15 +0300

    add directory images/images and put tree back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require tree .
+
 //= require jquery
 //= require jquery_ujs
 //= require jquery.raty
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require tree .
